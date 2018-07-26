ce024f02d8feea65d3eb7d244b2ed12c9702482e
Galina2018 <galstream@tut.by>
2018-07-26 19:12:46 +0300

    Commit first

---
+++ BSN/js/main.js
@@ -0,0 +1,25 @@
+$(document).ready(function(){
+
+    $('.go_to').click( function(){ // ловим клик по ссылке с классом go_to
+        var scroll_el = $(this).attr('href'); // возьмем содержимое атрибута href, должен быть селектором, т.е. например начинаться с # или .
+        if ($(scroll_el).length != 0) { // проверим существование элемента чтобы избежать ошибки
+            $('html, body').animate({ scrollTop: $(scroll_el).offset().top }, 500); // анимируем скроолинг к элементу scroll_el
+        }
+        return false; // выключаем стандартное действие
+    });
+
+	/* $("#form").submit(function() {
+		$.ajax({
+			type: "POST",
+			url: "js/mail.php",
+			data: $(this).serialize()
+		}).done(function() {
+			$(this).find("input").val("");
+			alert("Спасибо за заявку! Скоро мы с вами свяжемся.");
+			$("#form").trigger("reset");
+		});
+		return false;
+	}); */
+});
+
+		
